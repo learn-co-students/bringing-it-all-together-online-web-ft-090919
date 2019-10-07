17bfb47f582de9ce22f4ed562580a593f0dc8781
ewenaiwe <ewenaiwe@gmail.com>
2019-10-07 16:12:27 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,10 +19,12 @@ class Dog
     end
 
     def self.drop_table
-        sql = <<-SQL
-        DROP TABLE IF EXISTS dogs;
-        SQL
-        DB[:conn].execute(sql)
+        DB[:conn].execute("DROP TABLE IF EXISTS dogs;")
+    end
+
+    def update
+        sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
+        DB[:conn].execute(sql, @name, @breed, @id)
     end
 
     def save
@@ -40,10 +42,48 @@ class Dog
     end
 
     def self.create(name:, breed:)
-        dog = self.new(name: name, breed: breed)
-        dog.save
+        self.new(name: name, breed: breed).save
     end
 
+    def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+    end
 
+    def self.find_by_id(id)
+        sql = <<-SQL
+        SELECT *
+        FROM dogs
+        WHERE id = ?
+        SQL
+        dog = DB[:conn].execute(sql, id).flatten
+        self.new_from_db(dog)
+    end
+
+    def self.find_or_create_by(name:, breed:)
+        sql = <<-SQL
+        SELECT *
+        FROM dogs
+        WHERE name = ?
+        AND breed = ?
+        SQL
+        dog = DB[:conn].execute(sql, name, breed).flatten
+
+        if !dog.empty?
+            dog = self.new(id: dog[0], name: dog[1], breed: dog[2])
+        else
+            dog = self.create(name: name, breed: breed)
+        end
+        dog
+    end
+
+    def self.find_by_name(name)
+        sql = <<-SQL
+        SELECT *
+        FROM dogs
+        WHERE name = ?
+        SQL
+        dog = DB[:conn].execute(sql, name).flatten
+        self.new_from_db(dog)
+    end
 
 end
